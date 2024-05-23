bbf82f0a7bbf2d76c4b5fd0d4b976835c7b50caf
TatiZhang <Wenjiz1@uw.edu>
2024-05-23 16:23:10 -0700

    creating HW4 simulation Rscript and slurm script

---
+++ vignettes/HW4_simulation_execute.R
@@ -0,0 +1,17 @@
+
+library(UWBiost561)
+
+# Define the simulation parameters
+levels_n <- c(10, 20, 30, 40, 50) # Different sizes of the graph
+num_trials <- 10                   # Number of trials for each combination
+alpha_vec <- c(0.5, 0.75, 0.95)   # Different edge densities for the partial clique
+time_limit <- 30                  # Maximum time allowed for each implementation to run (in seconds)
+
+# Run the simulation study
+simulation_results <- run_simulation(levels_n, num_trials, alpha_vec, time_limit)
+
+# Save the simulation results to an .RData file
+save(simulation_results, file = "~/HW4_simulation.RData")
+
+# Optional: print a summary of the results to console
+print("Simulation study completed. Results have been saved to ~/HW4_simulation.RData.")
