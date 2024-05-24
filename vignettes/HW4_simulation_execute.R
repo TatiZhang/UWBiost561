347e72977ec425f7648cc4a489edd540896bd7e1
TatiZhang <Wenjiz1@uw.edu>
2024-05-23 17:01:13 -0700

    editing simulation scripts

--- vignettes/HW4_simulation_execute.R
+++ vignettes/HW4_simulation_execute.R
@@ -3,7 +3,7 @@ library(UWBiost561)
 
 # Define the simulation parameters
 levels_n <- c(10, 20, 30, 40, 50) # Different sizes of the graph
-num_trials <- 10                   # Number of trials for each combination
+num_trials <- 5                   # Number of trials for each combination
 alpha_vec <- c(0.5, 0.75, 0.95)   # Different edge densities for the partial clique
 time_limit <- 30                  # Maximum time allowed for each implementation to run (in seconds)
 
